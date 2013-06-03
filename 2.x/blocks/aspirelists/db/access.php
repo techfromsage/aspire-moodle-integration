54c9f28c9d8249a8c3b32d084b16758211719a88
Tim Hodson <tim@timhodson.com>
2013-06-03 17:30:36 +0100

    Added additional language strings. Also made sure this would be recognised as UTF-8

---
+++ 2.x/blocks/aspirelists/db/access.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: txh
+ * Date: 03/06/2013
+ * Time: 11:42
+ * To change this template use File | Settings | File Templates.
+ */
--- 2.x/blocks/aspirelists/lang/en/block_aspirelists.php
+++ 2.x/blocks/aspirelists/lang/en/block_aspirelists.php
@@ -4,15 +4,29 @@
 
 $string['pluginname'] = 'Talis Aspire Resource Lists';
 $string['aspirelists'] = 'Resource Lists';
+$string['no_base_url_configured'] = 'Talis Aspire base URL not configured. Contact the system administrator.';
 $string['no_resource_lists_msg'] = 'No resource lists found for';
 
 $string['config_targetAspire'] = 'Target Aspire URL';
 $string['config_targetAspire_desc'] = 'Enter the target Talis Aspire Campus Edition base URL. IMPORTANT: Do not add a trailing slash to the URL.';
 $string['config_targetAspire_default'] = 'Default: http://demo.talisaspire.com';
 $string['config_targetAspire_ex'] = 'http://demo.talisaspire.com';
+
 $string['config_kg'] = 'Target knowledge group';
-$string['config_kg_desc'] = 'Choose target knowledge grouping level you want to link at, e.g. course or module. This terminology should match that implemented in your Talis Aspire hierarchy.';
+$string['config_kg_desc'] = 'Choose target knowledge grouping level you want to link at, e.g. course or module. <br />This terminology should match that implemented in your Talis Aspire hierarchy.';
 $string['config_kg_ex'] = 'Default: modules';
+
+$string['config_moduleCodeRegex'] = 'Module Code Regex';
+$string['config_moduleCodeRegex_desc'] = 'A regular expression which will capture the module code part of a Moodle Course ID. <br />This will be mapped to the module code as defined in Talis Aspire, the regex pattern must have a single group which will be the part of the course id which is used. The default would match ABC123_201213 and provide us with the value ABC123 in the regex group';
+$string['config_moduleCodeRegex_ex'] = '^([A-Za-z0-9]{6})_[0-9]{6}$' ;
+
+$string['config_timePeriodRegex'] = 'Time Period Regex';
+$string['config_timePeriodRegex_desc'] = 'A regular expression which will capture the time period part of a Moodle Course ID. <br />This will be mapped to the Time Period slug as defined in Talis Aspire, the regex pattern must have a single group which will be the part of the course id which is used. The default would match ABC123_201213 and provide us with the value 201213 in the regex group';
+$string['config_timePeriodRegex_ex'] = '^[A-Za-z0-9]{6}_([0-9]{6})$' ;
+
+$string['config_timePeriodMapping'] = 'Time Period Mapping';
+$string['config_timePeriodMapping_desc'] = 'A JSON object describing how moodle time periods map to Talis Aspire time period slugs. <br />The form is key value pairs separated by commas {"moodleTimePeriodCode":"talisAspireTimePeriodCode"}';
+$string['config_timePeriodMapping_ex'] = '{"201213":"summer-2013","201314":"autumn-2013"}';
 
 $string['modules'] = 'Modules';
 $string['courses'] = 'Courses';
@@ -20,4 +34,12 @@ $string['units'] = 'Units';
 $string['programmes'] = 'Programmes';
 $string['subjects'] = 'Subjects';
 
-?>
+// singular or plurals for displaying the number of items on a list
+$string['item'] = 'item';
+$string['items'] = 'items';
+
+// label for use when showing date the list was last updated
+$string['lastUpdated'] = 'last updated';
+
+// added this to prove this was really a UTF-8 FILE!! on a mac 'file filename.txt' reports a UTF-8 file as ASCII if there are NO diacritics in the file!
+$spuriousVar = 'î';
