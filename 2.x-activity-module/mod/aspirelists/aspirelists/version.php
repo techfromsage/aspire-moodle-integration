5bc89986c5c311dc2ccf609bec6d0872a43cf4fa
Ross Singer <rossfsinger@gmail.com>
2014-04-30 17:12:31 -0400

    move the directory down.

--- 2.x-activity-module/mod/aspirelists/aspirelists/version.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$module->version   = 2014042301;
-$module->requires  = 2012062507; // See http://docs.moodle.org/dev/Moodle_Versions
-$module->cron      = 0;
-$module->component = 'mod_aspirelists';
-$module->maturity  = MATURITY_ALPHA;
-$module->release   = '.0001';
