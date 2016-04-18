4f54412e80a03b1400ccb3070b43c56de23500e7
Mark Sharp <m.sharp@chi.ac.uk>
2016-04-18 15:08:57 +0100

    added plugin var instead of module var

--- 2.x-activity-module/mod/aspirelists/version.php
+++ 2.x-activity-module/mod/aspirelists/version.php
@@ -1,9 +1,24 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2015112400;
-$module->requires  = 2012062507; // See http://docs.moodle.org/dev/Moodle_Versions
-$module->cron      = 0;
-$module->component = 'mod_aspirelists';
-$module->maturity  = MATURITY_ALPHA;
-$module->release   = '.0001';
+if (!isset($plugin)) {
+    // Avoid warning message in M2.5 and below.
+    $plugin = new stdClass();
+}
+
+$plugin->version   = 2015112400;
+$plugin->requires  = 2012062507; // See http://docs.moodle.org/dev/Moodle_Versions
+$plugin->cron      = 0;
+$plugin->component = 'mod_aspirelists';
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '.0001';
+
+if ($CFG->branch < 26) {
+    // Used by M2.5 and below.
+    $module->version = $plugin->version;
+    $module->cron = $plugin->cron;
+    $module->maturity = $plugin->maturity;
+    $module->release = $plugin->release;
+    $module->requires = $plugin->requires;
+    $module->component = $plugin->component;
+}
