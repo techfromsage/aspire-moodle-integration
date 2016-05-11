7fc197c68ed8f36dd48bd20c852d07c9e116fa5d
Richard Tattersall <lordtatty@googlemail.com>
2016-05-11 23:14:05 +0100

    Add new version number

--- 2.x-activity-module/mod/aspirelists/version.php
+++ 2.x-activity-module/mod/aspirelists/version.php
@@ -1,12 +1,13 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
+global $CFG;
 
 if (!isset($plugin)) {
     // Avoid warning message in M2.5 and below.
     $plugin = new stdClass();
 }
 
-$plugin->version   = 2015112400;
+$plugin->version   = 2016051100;
 $plugin->requires  = 2012062507; // See http://docs.moodle.org/dev/Moodle_Versions
 $plugin->cron      = 0;
 $plugin->component = 'mod_aspirelists';
@@ -14,7 +15,7 @@ $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '.0001';
 
 if ($CFG->branch < 26) {
-    // Used by M2.5 and below.
+    // Used by Moodle 2.5 and below.
     $module->version = $plugin->version;
     $module->cron = $plugin->cron;
     $module->maturity = $plugin->maturity;
