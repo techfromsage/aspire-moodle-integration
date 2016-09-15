2bcc4dc649320341d26c472ea2af328734606544
Richard Tattersall <lordtatty@googlemail.com>
2016-09-15 14:16:35 +0100

    spiked changes

--- 2.x-activity-module/mod/aspirelists/yui/src/inline_display/js/inline_display.js
+++ 2.x-activity-module/mod/aspirelists/yui/src/inline_display/js/inline_display.js
@@ -4,8 +4,10 @@ NS = M.mod_aspirelists.inline_display = {};
 NS.init_view = function(accordionOpen, accordionClosed) {
     Y.delegate('click', this.toggle_inline_list, Y.config.doc, '.aspirelists_inline_readings_toggle .activityinstance a', this);
     Y.on('domready', this.resize_embedded_lists);
+    Y.on('domready', this.load_queued_iframes);
     this.accordionOpen = accordionOpen;
     this.accordionClosed = accordionClosed;
+    this.iframeQueue = [];
 };
 
 NS.toggleAccordionIndicator = function(elem, indicatorText)
@@ -37,6 +39,7 @@ NS.toggle_inline_list = function(e)
 
 NS.resize_embedded_lists = function(e)
 {
+    console.log("resizeing");
     Y.all('.aspirelists_inline_list').each(function(o){
         var width = o.ancestor('.aspirelists').get("offsetWidth");
 
@@ -56,4 +59,52 @@ NS.resize_embedded_lists = function(e)
 
         o.setAttribute('width', width);
     });
+}
+
+NS.add_to_iframe_load_queue = function(src, element){
+    this.iframeQueue.push({'src':src, 'element': element})
+}
+
+NS.processNextOnQueue = function(){
+    var iframeData = this.iframeQueue.shift();
+    console.log(iframeData);
+    var src = iframeData['src'];
+    var element = iframeData['element'];
+    element.setAttribute('src', src);
+    return element;
+}
+
+NS.waitForIframeToLoad = function(element, callback) {
+    console.log(element);
+    console.log("--")
+    var win = Y.Node.getDOMNode(element.get('contentWindow'));
+    var iframeDoc = win.document;
+    // Check if loading is complete
+    if (iframeDoc.readyState == 'complete') {
+        win.onload = function () {
+            console.log("done a thing");
+            alert("hello");
+            return callback();
+        }
+    }
+    else {
+        // If we are here, it is not loaded. Set things up so we check   the status again in 100 milliseconds
+        window.setTimeout(NS.waitForIframeToLoad(element, callback), 100);
+    }
+}
+
+NS.processQueue = function() {
+    var element = NS.processNextOnQueue();
+    NS.waitForIframeToLoad(element, function(){
+        NS.processNextOnQueue();
+    });
+}
+
+NS.load_queued_iframes = function(e){
+    console.log("here");
+    Y.all('.aspirelists_inline_list').each(function (o) {
+        var src = o.getData('intended-src');
+        NS.add_to_iframe_load_queue(src, o);
+    });
+    NS.processQueue();
 }
