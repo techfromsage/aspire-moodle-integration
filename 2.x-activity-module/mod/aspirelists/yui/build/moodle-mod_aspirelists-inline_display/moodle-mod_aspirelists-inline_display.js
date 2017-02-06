27f86521ba996d3444383dfbfbf339625b492425
Richard Tattersall <lordtatty@googlemail.com>
2017-02-06 16:59:56 +0000

    Tidied up the code a lot

--- 2.x-activity-module/mod/aspirelists/yui/build/moodle-mod_aspirelists-inline_display/moodle-mod_aspirelists-inline_display.js
+++ 2.x-activity-module/mod/aspirelists/yui/build/moodle-mod_aspirelists-inline_display/moodle-mod_aspirelists-inline_display.js
@@ -3,15 +3,12 @@ YUI.add('moodle-mod_aspirelists-inline_display', function (Y, NAME) {
 M.mod_aspirelists = M.mod_aspirelists || {};
 NS = M.mod_aspirelists.inline_display = {};
 
-NS.inline_display_delay = 1000;
-
 NS.init_view = function(accordionOpen, accordionClosed) {
     Y.delegate('click', this.toggle_inline_list, Y.config.doc, '.aspirelists_inline_readings_toggle .activityinstance a', this);
     Y.on('domready', this.resize_embedded_lists);
-    Y.on('domready', this.load_queued_iframes);
+    Y.on('domready', this.queueInlineIFrames.init());
     this.accordionOpen = accordionOpen;
     this.accordionClosed = accordionClosed;
-    this.iframeQueue = [];
 };
 
 NS.toggleAccordionIndicator = function(elem, indicatorText)
@@ -64,37 +61,43 @@ NS.resize_embedded_lists = function(e)
     });
 };
 
-NS.add_to_iframe_load_queue = function(src, element){
-    this.iframeQueue.push({'src':src, 'element': element});
-};
-
-NS.populateIFrame = function(iFrameData){
-    var src = iFrameData.src;
-    var element = iFrameData.element;
-    element.setAttribute('src', src);
-};
-
-NS.processNextInQueue = function() {
-    console.log("Calling");
-    // Process the first launch call
-    var iFrameData = this.iframeQueue.shift();
-    if(iFrameData === undefined){
-        // In this case we have called all elements to load and can complete
-        return;
+/**
+ *
+ */
+NS.queueInlineIFrames = {
+    iframeQueue: [],
+    inline_display_delay: 1000,
+    add_to_iframe_load_queue: function(src, element){
+        this.iframeQueue.push({'src':src, 'element': element});
+    },
+    populateIFrame: function(iFrameData){
+        var src = iFrameData.src;
+        var element = iFrameData.element;
+        element.setAttribute('src', src);
+    },
+    processNextInQueue: function() {
+        // Process the first launch call
+        var iFrameData = this.iframeQueue.shift();
+        if(iFrameData === undefined){
+            // In this case we have called all elements to load and can complete
+            return;
+        }
+        this.populateIFrame(iFrameData);
+        // Set up a timeout to continue iterating over the calls
+        var queueIframeScope = this;
+        window.setTimeout(function(){
+            queueIframeScope.processNextInQueue();
+        }, this.inline_display_delay);
+    },
+    init: function(){
+        var queueIframeScope = this;
+        // Add all inline elements to the queue;
+        Y.all('.aspirelists_inline_list').each(function (o) {
+            var src = o.getData('intended-src');
+            queueIframeScope.add_to_iframe_load_queue(src, o);
+        });
+        this.processNextInQueue();
     }
-    NS.populateIFrame(iFrameData);
-    // Set up a timeout to continue iterating over the calls
-    window.setTimeout(function(){
-        NS.processNextInQueue();
-    }, NS.inline_display_delay);
-};
-
-NS.load_queued_iframes = function(e){
-    Y.all('.aspirelists_inline_list').each(function (o) {
-        var src = o.getData('intended-src');
-        NS.add_to_iframe_load_queue(src, o);
-    });
-    NS.processNextInQueue();
 };
 
 }, '@VERSION@', {"requires": ["base", "node", "event", "event-delegate"]});
