210ac554dd4bd812606f733e55114a5609392c0c
Tim Hodson <tim@timhodson.com>
2019-10-01 13:24:49 +0100

    Remove php 5.6. move provider

---
+++ moodle-activity-module-lti-wrapper/mod/aspirelists/classes/privacy/provider.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace aspirelists\privacy;
+use core_privacy\local\metadata\collection;
+use core_privacy\local\request\approved_contextlist;
+use core_privacy\local\request\approved_userlist;
+use core_privacy\local\request\context;
+use core_privacy\local\request\contextlist;
+use core_privacy\local\request\userlist;
+use mod_assign\privacy\useridlist;
+
+/**
+ * This plugin does not store any personal user data in the database, but does cause LTI connections to be made.
+ */
+class provider implements
+    \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\core_userlist_provider,
+    \core_privacy\local\request\plugin\provider {
+
+    /**
+     * Get the language string to show as a reason.
+     * @return string
+     */
+    public static function get_reason() : string {
+        return 'privacy:metadata:reason';
+    }
+
+    /**
+     * Get a list of the metadata used
+     *
+     * @param collection $collection
+     *
+     * @return collection
+     */
+    public static function get_metadata(collection $collection) : collection {
+
+        $collection->add_external_location_link('lti_client', [
+            'userid' => 'privacy:metadata:lti_client:userid',
+            'role' => 'privacy:metadata:lti_client:role',
+            'courseid' => 'privacy:metadata:lti_client:courseid',
+            'courseidnumber' => 'privacy:metadata:lti_client:courseidnumber',
+            'courseshortname' => 'privacy:metadata:lti_client:courseshortname',
+            'coursefullname' => 'privacy:metadata:lti_client:coursefullname',
+        ], 'privacy:metadata:lti_client');
+
+        return $collection;
+    }
+
+
+    /**
+     * Get the list of contexts that contain user information for the specified user.
+     *
+     * @param int $userid The user to search.
+     *
+     * @return  contextlist   $contextlist  The contextlist containing the list of contexts used in this plugin.
+     */
+    public static function get_contexts_for_userid(int $userid): contextlist
+    {
+        // Not required as mod_lti will handle this
+    }
+
+    /**
+     * Export all user data for the specified user, in the specified contexts.
+     *
+     * @param approved_contextlist $contextlist The approved contexts to export information for.
+     */
+    public static function export_user_data(approved_contextlist $contextlist)
+    {
+        // Not required as mod_lti will handle this
+    }
+
+    /**
+     * Delete all data for all users in the specified context.
+     *
+     * @param context $context The specific context to delete data for.
+     */
+    public static function delete_data_for_all_users_in_context(\context $context)
+    {
+        // Not required as mod_lti will handle this
+    }
+
+    /**
+     * Delete all user data for the specified user, in the specified contexts.
+     *
+     * @param approved_contextlist $contextlist The approved contexts and user information to delete information for.
+     */
+    public static function delete_data_for_user(approved_contextlist $contextlist)
+    {
+        // Not required as mod_lti will handle this
+    }
+
+    /**
+     * Get the list of users who have data within a context.
+     *
+     * @param userlist $userlist The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(userlist $userlist)
+    {
+        // Not required as mod_lti will handle this
+    }
+
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param approved_userlist $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(approved_userlist $userlist)
+    {
+        // Not required as mod_lti will handle this
+    }
+}
